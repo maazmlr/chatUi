18f8d13342c4c3e99ea8bc5c39992174427e0c71
muhammad maaz <compiletimeterror@gmail.com>
2024-03-14 22:49:23 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import Form from './modules/Form/Form'
 import Dashboard from './modules/Dashboard/Dashboard'
-import { createBrowserRouter,createRoutesFromElements ,Route ,RouterProvider } from 'react-router-dom'
+import { createBrowserRouter,createRoutesFromElements ,Navigate,Route ,RouterProvider, Routes } from 'react-router-dom'
 
 const App = () => {
 
+
+  const ProtectedRoute=({children})=>{
+    const isLoggedIn=localStorage.getItem("user:token") !==null;
+
+    // if(!isLoggedIn){
+    //   return <Navigate to={"/signin"}/>
+    // }
+    return children
+  }
+
   const router = createBrowserRouter(createRoutesFromElements(
     <Route>
-      <Route path='/' element={<Dashboard/>}/>
+      <Route path='/' element={
+        <ProtectedRoute>
+          <Dashboard/>
+        </ProtectedRoute>
+      }/>
 
       <Route path='/signup' element={<Form isSignIn={false}/> }/>
       <Route path='/signin' element={<Form isSignIn={true}/> }/>
--- src/modules/Form/Form.jsx
+++ src/modules/Form/Form.jsx
@@ -1,47 +1,76 @@
-import React, { useState } from 'react'
-import Input from '../../components/Input'
-import Button from '../../components/Button'
+import React, { useState } from "react";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
-const Form = ({
-    isSignIn=false
-
-}) => {
-  const [data,setData]=useState({
+const Form = ({ isSignIn = false }) => {
+  const [data, setData] = useState({
     ...(isSignIn && {
-      fullName:""
+      fullName: "",
     }),
-    email:'',
-    password:''
-  })
-
- 
+    email: "",
+    password: "",
+  });
 
   return (
-    <div className='w-[30em] h-[30rem]  bg-primary shadow-lg rounded-lg flex flex-col items-center justify-center '>
-    <h2 className='text-3xl text-accent font-bold mb-14  '>Welcome</h2>
+    <div className="w-screen h-screen flex justify-center items-center">
+    <div className="w-[30em] h-[30rem]    bg-gray-200 shadow-2xl rounded-lg flex flex-col items-center justify-center ">
+      <h2 className="text-3xl text-accent font-bold mb-14  ">Welcome</h2>
 
-    <div className="fields">
-    {!isSignIn && <Input label='Full Name' className='mb-6' value={data.fullName}  onChange={(e)=>setData({
-      ...data,
-      fullName:e.target.value
-    })}/> }
-    <form action="">
-
-    <Input label='Email' type='email' className='mb-6' value={data.email} onChange={(e)=>setData({
-      ...data,
-      email:e.target.value
-    })}/>
-    <Input label='password' type='password' className='mb-6' value={data.password}  onChange={(e)=>setData({
-      ...data,
-      password:e.target.value
-    })}  /> 
-    <Button text={isSignIn ?'Sign in ' :'Signup'} type='submit'  className='inline-block mx-auto'/>
-
-    </form>
-    <p className='text-center mt-4'>{isSignIn ? "Don't have an account" :"Already Have an account" } <span className='underline cursor-pointer text-blue-600 '>{isSignIn ?"Singup ": "Sign in"}</span></p>
+      <div className="fields">
+        {!isSignIn && (
+          <Input
+            label="Full Name"
+            className="mb-6"
+            value={data.fullName}
+            onChange={(e) =>
+              setData({
+                ...data,
+                fullName: e.target.value,
+              })
+            }
+          />
+        )}
+        <form action="">
+          <Input
+            label="Email"
+            type="email"
+            className="mb-6"
+            value={data.email}
+            onChange={(e) =>
+              setData({
+                ...data,
+                email: e.target.value,
+              })
+            }
+          />
+          <Input
+            label="password"
+            type="password"
+            className="mb-6"
+            value={data.password}
+            onChange={(e) =>
+              setData({
+                ...data,
+                password: e.target.value,
+              })
+            }
+          />
+          <Button
+            text={isSignIn ? "Sign in " : "Signup"}
+            type="submit"
+            className="inline-block mx-auto"
+          />
+        </form>
+        <p className="text-center mt-4">
+          {isSignIn ? "Don't have an account" : "Already Have an account"}{" "}
+          <span className="underline cursor-pointer text-blue-600 ">
+            {isSignIn ? "Singup " : "Sign in"}
+          </span>
+        </p>
+      </div>
     </div>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
